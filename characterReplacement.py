04ea0bd951733a8c6164be32583fa083847a9872
c.huang <chuang@netprotections.co.jp>
2023-12-12 22:57:29 +0800

    fix: change dict from default dict

--- characterReplacement.py
+++ characterReplacement.py
@@ -3,18 +3,21 @@ from collections import defaultdict
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        res = 0
-
+        alphCount = defaultdict(int)
         l = 0
+        res = 0
+        
         for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-
-            while (r - l + 1) - max(count.values()) > k:
-                count[s[l]] -= 1
+            alphCount[s[r]] += 1
+            
+            while r - l + 1 - max(alphCount.values()) > k:
+                alphCount[s[l]] -= 1
                 l += 1
+
             res = max(res, r - l + 1)
+
         return res
+
 
 solution = Solution()
 s = "ABAB"
