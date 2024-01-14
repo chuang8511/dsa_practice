bcce19fd581e2f952be82a7d18c8357de9cff9e2
c.huang <chuang@netprotections.co.jp>
2024-01-14 21:13:26 +0800

    chore: make file generation easier

---
+++ file_generator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Declare variables to capture argument values
+ProblemType="$1"
+PlatformNumber="$2"
+
+# Create the directory structure with appropriate formatting
+mkdir "${ProblemType}/${PlatformNumber}"  # Create parent and child directories together
+
+# Create empty Python files with proper extensions
+touch "${ProblemType}/${PlatformNumber}/solution.py"
+touch "${ProblemType}/${PlatformNumber}/test_solution.py"
