8c100d5e1222c96208112373f53d37064c527ba3
c.huang <chuang@netprotections.co.jp>
2024-01-23 00:47:04 +0800

    fix: template of test sample code

--- test_code_sample.py
+++ test_code_sample.py
@@ -4,3 +4,12 @@ from solution import Solution
 class TestSolution(unittest.TestCase):
     def test_method(self):
         solution = Solution()
+
+        result = solution.test_method()
+        self.assertEqual(result, 0)
+
+        result = solution.test_method()
+        self.assertEqual(result, 0)
+
+        result = solution.test_method()
+        self.assertEqual(result, 0)
