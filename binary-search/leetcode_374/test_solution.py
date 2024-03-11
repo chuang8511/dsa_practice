150d9ca5fc9317c9fd6ee5024e75db03491239be
chuang8511 <a0961192312@gmail.com>
2024-03-11 14:59:18 +0800

    temp: to be added mock test code

---
+++ binary-search/leetcode_374/test_solution.py
@@ -0,0 +1,15 @@
+import unittest
+from solution import Solution
+
+class TestSolution(unittest.TestCase):
+    def test_method(self):
+        solution = Solution()
+
+        result = solution.method()
+        self.assertEqual(result, 0)
+
+        result = solution.method()
+        self.assertEqual(result, 0)
+
+        result = solution.method()
+        self.assertEqual(result, 0)
