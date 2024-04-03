0d6f681da0d737001f12aeb3a9d3dcaed4b75fab
chuang8511 <a0961192312@gmail.com>
2024-04-03 12:43:18 +0100

    feat: brute force for coin change

---
+++ 1d-dp/creativeX.py
@@ -0,0 +1,60 @@
+# // https://codeinterview.io/PJBWBZQEWS
+# // Inputs:
+# // Coins: [1,2,3]
+# // Sum: 5
+# //
+# // Expected Output:
+# // [
+# // [1, 1, 1, 1, 1],
+# // [2, 3],
+# // [1, 1, 3],
+# // [1, 1, 1, 2],
+# // [1, 2, 2],
+# // ]
+class Soultion:
+  def solution(self, sum: int, coins: list):
+    self.res = []
+    
+    coins.sort()
+    possible_solution = []
+    i = len(coins) - 1
+    while sum > 0 and i >= 0:
+      if coins[i] > sum:
+        i -= 1
+      else:
+        sum -= coins[i]
+        possible_solution.append(coins[i])
+    
+    possible_solution.sort()
+    self.res.append(possible_solution) # [3, 2]
+    
+    if sum != 0:
+      return []
+    
+    j = 0
+    while j < len(self.res):
+      result_to_be_break_down = self.res[j]
+      for num in result_to_be_break_down:
+        copy_solution = result_to_be_break_down.copy()
+        copy_solution.remove(num)
+        this_coin = num
+        
+        i = coins.index(num) - 1 # optimize the loop
+        while num > 0 and i >= 0:
+          if this_coin == coins[i]:
+            i -= 1
+          elif coins[i] > num:
+            i -= 1
+          else:
+            num -= coins[i]
+            copy_solution.append(coins[i])
+        copy_solution.sort()
+        if copy_solution and num == 0 and copy_solution not in self.res:
+          self.res.append(copy_solution)
+      j += 1
+
+    return self.res
+    
+      
+a = Soultion()
+print(a.solution(5, [1,2,3]))
